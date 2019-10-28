fe92d40a1bbf21332dd84272a706a0c0cec9693d
Lozano Elian <Elian.lozano@outlook.fr>
2019-10-28 13:33:17 +0100

    commit 2

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 $(document).ready(function () {
-    $("input").attr("minlength", "5")
 
-    $("#formulaire").validate({
-        rules: {
-            password: "required",
-            confirmpassword: {
-                equalTo: "#password"
-            }
-        },
+    $("#btnconfirm").click(function () {
+
+        $("input").attr("minlength", "5")
+        validation()
+
     })
+    $('#formulaire').keyup(function () {
+        var password = $('#password');
+        var confirm = $('#password_confirm');
+        if (password.val() == confirm.val()) {
+            password.css('border', '3px solid green');
+            confirm.css('border', '3px solid green');
+        } else {
+            confirm.css('border', '3px solid red');
+        }
+        if ($("input").val() == ("input").html("")) {
+            alert("Rempli tout les champs")
+    
+        }
+    });
+
 })
+
+function validation() {
+    var password = $('#password');
+    var confirm = $('#password_confirm');
+    if (password.val() == confirm.val()) {
+        console.log("succes");
+
+    } else {
+        console.log("pas pareil");
+
+    }
+}
